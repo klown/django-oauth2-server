0881e2f6ce1a4311664dd4162a8a0f904642cb72
Joseph Scheuhammer <clown@alum.mit.edu>
2021-01-23 01:20:47 -0500

    OAuth2 tutorial part 2 - "Protecting your APIs"
    If the OAuth2 server is also a resource server, the resources will
    not be returned without passing an Access token when requesting the
    resource.

--- auth/accounts/accounts/views.py
+++ auth/accounts/accounts/views.py
@@ -5,7 +5,18 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
 
+# oauth2
+from oauth2_provider.views.generic import ProtectedResourceView
+from django.http import HttpResponse
+
+# new2
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+# oauth2
+class ApiEndpoint(ProtectedResourceView):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse('Hello, OAuth2!')
+
--- auth/accounts/config/urls.py
+++ auth/accounts/config/urls.py
@@ -18,9 +18,40 @@ from django.urls import include, path
 
 from django.views.generic.base import TemplateView # new
 
+# oauth2
+from django.conf.urls import url, include
+import oauth2_provider.views as oauth2_views
+from django.conf import settings
+from accounts.views import ApiEndpoint
+
+# OAuth2 provider endpoints
+oauth2_endpoint_views = [
+    path('authorize/', oauth2_views.AuthorizationView.as_view(), name="authorize"),
+    path('token/', oauth2_views.TokenView.as_view(), name="token"),
+    path('revoke-token/', oauth2_views.RevokeTokenView.as_view(), name="revoke-token"),
+]
+
+if settings.DEBUG:
+    # OAuth2 Application Management endpoints
+    oauth2_endpoint_views += [
+        path('applications/', oauth2_views.ApplicationList.as_view(), name="list"),
+        path('applications/register/', oauth2_views.ApplicationRegistration.as_view(), name="register"),
+        path('applications/<pk>/', oauth2_views.ApplicationDetail.as_view(), name="detail"),
+        path('applications/<pk>/delete/', oauth2_views.ApplicationDelete.as_view(), name="delete"),
+        path('applications/<pk>/update/', oauth2_views.ApplicationUpdate.as_view(), name="update"),
+    ]
+
+    # OAuth2 Token Management endpoints
+    oauth2_endpoint_views += [
+        path('authorized-tokens/', oauth2_views.AuthorizedTokensListView.as_view(), name="authorized-token-list"),
+        path('authorized-tokens/<pk>/delete/', oauth2_views.AuthorizedTokenDeleteView.as_view(), name="authorized-token-delete"),
+    ]
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("o/", include('oauth2_provider.urls', namespace='oauth2_provider')), # oauth2
+#    path("o/", include('oauth2_provider.urls', namespace='oauth2_provider')), # oauth2
+    path('o/', include((oauth2_endpoint_views, 'oauth'), namespace="oauth2_provider")), # oauth2
+    path('api/hello', ApiEndpoint.as_view()),  # oauth2: an example resource endpoint
     path('accounts/', include('accounts.urls')), # new2
     path('accounts/', include('django.contrib.auth.urls')), # new
     path('', TemplateView.as_view(template_name='home.html'), name='home'), # new    
